185cc6d03c21e942d6858569f1daef45c7dbe1a5
irike1 <134058814+irike1@users.noreply.github.com>
2024-08-03 21:05:30 -0500

    Alpha commit?

--- components/ws2805/ws2805.cpp
+++ components/ws2805/ws2805.cpp
@@ -1,9 +1,46 @@
-#include "ws2805"
+#include "ws2805.h"
 
-void YourComponentName::setup() {
-  // Setup code here
+void WS2805::setup() {
+  // Initialize GPIO pin, assume using GPIO5 for data
+  pinMode(5, OUTPUT);
+  digitalWrite(5, LOW);
 }
 
-void YourComponentName::loop() {
-  // Loop code here
+void WS2805::write_state(LightState *state) {
+  // Get the current color from the light state
+  float red, green, blue, white1, white2;
+  state->current_values_as_rgbw(&red, &green, &blue, &white1, &white2);
+
+  // Convert to 8-bit values
+  uint8_t r = red * 255.0;
+  uint8_t g = green * 255.0;
+  uint8_t b = blue * 255.0;
+  uint8_t w1 = white1 * 255.0;
+  uint8_t w2 = white2 * 255.0;
+
+  // Send color to LED strip
+  send_color(r, g, b, w1, w2);
 }
+
+void WS2805::send_color(uint8_t r, uint8_t g, uint8_t b, uint8_t w1, uint8_t w2) {
+  // Send 40 bits: 8 bits each for R, G, B, W1, W2
+  send_byte(r);
+  send_byte(g);
+  send_byte(b);
+  send_byte(w1);
+  send_byte(w2);
+
+  // Send the reset signal (low for 280us)
+  digitalWrite(5, LOW);
+  delayMicroseconds(280);
+}
+
+void WS2805::send_bit(bool bit_val) {
+  if (bit_val) {
+    digitalWrite(5, HIGH);
+    delayMicroseconds(580);  // T1H: 580ns to 1us
+    digitalWrite(5, LOW);
+    delayMicroseconds(580);  // T1L: 580ns to 1us
+  } else {
+    digitalWrite(5, HIGH);
+    delayMicroseconds(2
--- components/ws2805/ws2805.h
+++ components/ws2805/ws2805.h
@@ -2,13 +2,13 @@
 
 #include "esphome.h"
 
-class YourComponentName : public Component, public Sensor {
+class WS2805 : public Component, public LightOutput {
 public:
-  void setup() override {
-    // This will be called by ESPHome when the component is initialized
-  }
+  void setup() override;
+  void write_state(LightState *state) override;
 
-  void loop() override {
-    // This will be called by ESPHome every loop iteration
-  }
+private:
+  void send_color(uint8_t r, uint8_t g, uint8_t b, uint8_t w1, uint8_t w2);
+  void send_bit(bool bit_val);
+  void send_byte(uint8_t byte);
 };
