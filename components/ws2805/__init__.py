a6911d1d4a8a5391367cb76066ed35a694e9878d
irike1 <134058814+irike1@users.noreply.github.com>
2024-08-03 21:34:22 -0500

    added init.py

---
+++ components/ws2805/__init__.py
@@ -0,0 +1,21 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import light
+from esphome.const import CONF_ID, CONF_OUTPUT_ID
+
+DEPENDENCIES = ['light']
+
+CONF_WS2805_ID = 'ws2805_id'
+
+ws2805_ns = cg.esphome_ns.namespace('ws2805')
+WS2805 = ws2805_ns.class_('WS2805', light.LightOutput, cg.Component)
+
+CONFIG_SCHEMA = light.LIGHT_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(WS2805),
+    cv.Required(CONF_OUTPUT_ID): cv.use_id(WS2805),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_WS2805_ID])
+    yield cg.register_component(var, config)
+    yield light.register_light(var, config)
